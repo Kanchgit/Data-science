4b648cb4e7b40af5af58b0254bce37db97dfcf6d
Kanchgit <kanchanamurugesh@gmail.com>
2021-08-30 14:40:04 +0530

    My first commit abd push to cloud REPO

---
+++ Python/f2.py
@@ -0,0 +1,5 @@
+Teamstudio makes software products for organizations with customized HCL Notes and Domino applications that help them implement best practices, work more efficiently, and prepare for the future.
+
+Whether you're working with HCL Notes and Domino or moving away from it, our trusted tools provide a more productive and enjoyable experience.
+
+We are an HCL Business Partner and a privately held, international company headquartered in the United States with regional offices in the United Kingdom and Japan.
